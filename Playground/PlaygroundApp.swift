04b9b0d95780e47a65940d0b388354d8ec1192b2
Kamaal Farah <kamaal.f1@gmail.com>
2023-08-06 12:00:02 +0200

    Initializing playground

---
+++ Playground/PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlaygroundApp.swift
+//  Playground
+//
+//  Created by Kamaal Farah on 8/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
