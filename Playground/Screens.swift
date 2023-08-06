d210058907efd441ba9dd8a3dc068383bca4cf36
Kamaal Farah <kamaal.f1@gmail.com>
2023-08-06 12:26:40 +0200

    Initializing app logo creator

---
+++ Playground/Screens.swift
@@ -0,0 +1,12 @@
+//
+//  Screens.swift
+//  Playground
+//
+//  Created by Kamaal M Farah on 06/08/2023.
+//
+
+import Foundation
+
+enum Screens: Codable, Hashable {
+    case appLogoCreator
+}
---
+++ Playground/Views/Screens/AppLogoCreatorScreen.swift
@@ -0,0 +1,18 @@
+//
+//  AppLogoCreatorScreen.swift
+//  Playground
+//
+//  Created by Kamaal M Farah on 06/08/2023.
+//
+
+import SwiftUI
+
+struct AppLogoCreatorScreen: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    AppLogoCreatorScreen()
+}
